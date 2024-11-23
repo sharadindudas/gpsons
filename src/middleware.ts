f9d06244744f835e0aaad616c58aaecd799eac6f
sharadindu das <sharadindudas774@gmail.com>
2024-11-23 13:18:00 +0530

    fix: fixed the middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,34 +1,27 @@
 import { NextResponse, NextRequest } from "next/server";
 
 export const config = {
-  matcher: [
-    "/",
-    "/about",
-    "/contact",
-    "/cart",
-    "/checkout/:path*",
-    "/product/:path*",
-    "/profile/:path*",
-    "/filter/:path*",
-    "/privacy-policy",
-    "/return-policy",
-    "/shipping-policy",
-    "/terms-and-conditions",
-    "/faqs",
-  ],
+    matcher: [
+        "/",
+        "/cart",
+        "/checkout/:path*",
+        "/product/:path*",
+        "/profile/:path*",
+        "/filter/:path*"
+    ]
 };
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get("token")?.value;
-  const protectedRoutes = ["/cart", "/checkout", "/profile"];
-  const url = request.nextUrl;
+    const token = request.cookies.get("token")?.value;
+    const protectedRoutes = ["/cart", "/checkout", "/profile"];
+    const url = request.nextUrl;
 
-  if (
-    !token &&
-    protectedRoutes.some((route) => url.pathname.startsWith(route))
-  ) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
+    if (
+        !token &&
+        protectedRoutes.some((route) => url.pathname.startsWith(route))
+    ) {
+        return NextResponse.redirect(new URL("/", request.url));
+    }
 
-  return NextResponse.next();
+    return NextResponse.next();
 }
